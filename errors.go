40c729540f57de92c75d5c34caa7549c012f37cd
Denis Kuso <sourcework.online@keemail.me>
2024-06-10 21:01:07 +0100

    add custom error type for CI step

---
+++ errors.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrValidation = errors.New("validation failure")
+
+type stepErr struct {
+	// which CI step caused the error
+	step  string
+	msg   string
+	cause error
+}
+
+func (s *stepErr) Error() string {
+	return fmt.Sprintf("step: %q: %s: cause: %v", s.step, s.msg, s.cause)
+}
+
+func (s *stepErr) Is(target error) bool {
+	t, ok := target.(*stepErr)
+	if !ok {
+		return false
+	}
+	return t.step == s.step
+}
+
+func (s *stepErr) Unwrap() error {
+	return s.cause
+}
