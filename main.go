d435cd966f1182a3806ac7dc5254f816b620a31c
Denis Kuso <sourcework.online@keemail.me>
2024-06-13 15:49:53 +0100

    add handling of termination signals

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -25,6 +27,11 @@ func run(project string, out io.Writer) error {
 	if project == "" {
 		return fmt.Errorf("Project directory required: %w", ErrValidation)
 	}
+	const capacity int = 1
+	manager := make(chan os.Signal, capacity)
+	faults := make(chan error)
+	done := make(chan struct{})
+	signal.Notify(manager, syscall.SIGINT, syscall.SIGTERM)
 	const numStep int = 4 // TODO refactor pipe building
 	pipe := make([]executer, numStep)
 	pipe[0] = NewStep("go build", "go", []string{"build", ".", "errors"},
@@ -34,15 +41,31 @@ func run(project string, out io.Writer) error {
 	var sleep time.Duration = 5 * time.Second // arbitrary decision
 	pipe[3] = NewTimeoutStep("git push", "git", []string{"push", "origin", "master"}, "git push: SUCCESS", project, sleep)
 
-	for _, s := range pipe {
-		msg, err := s.execute()
-		if err != nil {
-			return err
+	go func() {
+		for _, s := range pipe {
+			msg, err := s.execute()
+			if err != nil {
+				faults <- err
+				return
+			}
+			_, err = fmt.Fprintln(out, msg)
+			if err != nil {
+				faults <- err
+				return
+			}
 		}
-		_, err = fmt.Fprintln(out, msg)
-		if err != nil {
+		close(done)
+	}()
+	for {
+		select {
+		case got := <-manager:
+			signal.Stop(manager)
+			return fmt.Errorf("\nStoping pipeline due to: %s", got)
+		case err := <-faults:
+			//fmt.Printf("poped %s from err chanel\n", err)
 			return err
+		case <-done:
+			return nil
 		}
 	}
-	return nil
 }
